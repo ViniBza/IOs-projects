1dd63d2932c33926f74ebeb1a3e70f0e2f28f8eb
ViniBza <ViniBza@gmail.com>
2025-02-15 15:23:46 +0000

    Initialize project

---
+++ I'm Poor/I'm Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I'm Poor
+//
+//  Created by Vinicio Barbosa Bezerra on 15/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ I'm Poor/I'm PoorTests/I_m_PoorTests.swift
@@ -0,0 +1,17 @@
+//
+//  I_m_PoorTests.swift
+//  I'm PoorTests
+//
+//  Created by Vinicio Barbosa Bezerra on 15/02/2025.
+//
+
+import Testing
+@testable import I_m_Poor
+
+struct I_m_PoorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
